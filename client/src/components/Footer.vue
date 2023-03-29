72e73ed6b4b4fc3a4dda80aac95bc57376f1095e
Sidney de Vries <sidney.de.vries96@gmail.com>
2023-03-29 23:49:31 +0200

    Added footer icon

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="footer">
+    <img :src="image" class="footer-image"/>
     <b-col>
       <p class="mb-0">CritterDB ©{{ year }}</p>
       <p><a href="https://github.com/seirveds/"><font-awesome-icon icon="fa-brands fa-github" />/seirveds</a></p>
@@ -13,6 +14,7 @@ export default {
   data() {
     return {
       year: new Date().getFullYear(),
+      image: require('@/assets/icons/footer_graphic.png'),  // eslint-disable-line
     };
   },
 };
