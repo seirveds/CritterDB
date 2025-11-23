6a91d8aee8662d7dba92e592fa99437bddc544c6
seirveds <46373011+seirveds@users.noreply.github.com>
2025-11-23 15:57:47 +0100

    Update App.svelte
    Default selected critters type is now bug

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@
 
     export let selectedGame = "newhorizons";
     export let filters = {
-        "crittertype": "fish",
+        "crittertype": "bug",
         "month": currentMonth(),
         "time": currentTime(),
         "all": false,
